1c48d05548dd10e7fe40f6d01432100b9e8a5527
Dennyyyyyyy <den.grinyko@gmail.com>
2024-01-22 20:55:44 +0200

    update config files

--- README.md
+++ README.md
@@ -1,95 +1,19 @@
-# kbot
-DevOps application from scratch 
+# w9task1 Open-telemetry observability.  K8s+OTEL
 
-https://t.me/Dennyyyyyyy_bot
+Sample configuration for Kbot that send logs to [OpenTelemetry Collector] and metrics to [OpenTelemetry Collector] or [Prometheus].
 
-#ініціалузуємо mod над яким будемо працювати далі
-go mod init github.com/Dennyyyyyyy/kbot #вказуємо імʼя репозиторія, go створить файл go.mod де буде зберігати всі модулі кода
+## Prerequisites
 
-#встановимо кодогенератор, потужна бібліотека для створення сучасних cli-додатків 
-go install github.com/spf13/cobra-cli@latest 
+- [Docker]
+- [Docker Compose]
 
-#згенеруємо початковий код
-cobra-cli init #буде створено основний файл main.go, заповнить go.mod, та директорію cmd з root.go де згенеровано початковий код
+## How to run
 
-#згенеруємо код команди version
-cobra-cli add version #отримали новий файл version.go
-
-#білд програми, команда run автоматично білдає і запускає код
-go run main.go help #як видно код програми help вже інтегровано
-
-#перевіримо команду version
-go run main.go version 
-
-#додамо команду яка буде з основним кодом
-cobra-cli add kbot
-
-#ця команда динамічно збирає всі компоненти перед білдом (ld - це лінкер), прямо з командного рядка. Параметр -Х присвоєння значення зміним в модулях.
-go build -ldflags "-X="github.com/Dennyyyyyyy/kbot/cmd.appVersion=1.0.0 #отримали бінарний файл на імʼя kbot
-
-#запустимо наш kbot
-./kbot
-
-#імпортуємо пакет в kbot.go
-telebot "gopkg.in/telebot.v3"
-
-#декларуємо зміну TELE_TOKEN
-var (
-	//name of Telebot
-	TeleToken = os.Getenv("TELE_TOKEN")
-)
-
-#додамо в блоці хендлера kbot код функції run
-fmt.Printf("kbot %s started", appVersion)
-
-#та блок ініціалізації kbot, створення нового боту з параметрами
-kbot, err := telebot.NewBot(telebot.Settings{
-			URL:    "",
-			Token:  TeleToken,
-			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
-		})
-
-#наступний блок це блок обробки помилок
-if err != nil {
-			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
-			return
-		}
-
-#обробка події коду Handler-a
-kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
-			payload :=m.Message().Payload
-
-				switch payload {
-				case "Hello Denny":
-					err = m.Send (fmt.Sprintf("Hello! I`m Kbot %s. How are you up?", appVersion))
-					
-				}
-
-			return err
-		})
-
-#запуск бота
-kbot.Start()
-
-#переходимо до тестування
-gofmt -s -w ./     #форматує код де використовує таб для відступів,пропусків,вирівнювання
-
-#завантажемо пакети та залежності
-go get
-
-#білдуємо программу з новою версією
-go build -ldflags "-X="github.com/Dennyyyyyyy/kbot/cmd.appVersion=1.0.2
-
-#додамо токен в зміну TELE_TOKEN без збереження інформації в логах
-read -s -w TOKEN_TELE #далі вставимо наш токен Ctrl+V
-
-#єкспортуеємо зміну
-export TELE_TOKEN
-
-#запустимо програму та перевіримо
-./kbot start
-
-Чудово, все працює!
-
-Комміт та пуш.
+```bash
+export TELE_TOKEN=<TOKEN>
+docker-compose up
+```
+![1]()
+![2]()
+![3]()
+![4]()
--- cmd/kbot.go
+++ cmd/kbot.go
@@ -11,13 +11,64 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/hirosassa/zerodriver"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 	telebot "gopkg.in/telebot.v3"
 )
 
 var (
 	//name of Telebot
 	TeleToken = os.Getenv("TELE_TOKEN")
+	// MetricsHost exporter host:port
+	MetricsHost = os.Getenv("METRICS_HOST")
 )
+
+
+//Initialize OpenTelemetry
+func initMetrics(ctx context.Context) {
+
+	// Create a new OTLP Metric gRPC exporter with the specified endpoint and options
+	exporter, _ := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithEndpoint(MetricsHost),
+		otlpmetricgrpc.WithInsecure(),
+	)
+
+	// Define the resource with attributes that are common to all metrics.
+	// labels/tags/resources that are common to all metrics.
+	resource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String(fmt.Sprintf("kbot_%s", appVersion)),
+	)
+
+	// Create a new MeterProvider with the specified resource and reader
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(resource),
+		sdkmetric.WithReader(
+			// collects and exports metric data every 10 seconds.
+			sdkmetric.NewPeriodicReader(exporter, sdkmetric.WithInterval(10*time.Second)),
+		),
+	)
+
+	// Set the global MeterProvider to the newly created MeterProvider
+	otel.SetMeterProvider(mp)
+
+}
+
+func pmetrics(ctx context.Context, payload string) {
+	// Get the global MeterProvider and create a new Meter with the name "kbot_light_signal_counter"
+	meter := otel.GetMeterProvider().Meter("kbot_light_signal_counter")
+
+	// Get or create an Int64Counter instrument with the name "kbot_light_signal_<payload>"
+	counter, _ := meter.Int64Counter(fmt.Sprintf("kbot_light_signal_%s", payload))
+
+	// Add a value of 1 to the Int64Counter
+	counter.Add(ctx, 1)
+}
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
@@ -30,8 +81,9 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
 
+	Run: func(cmd *cobra.Command, args []string) {
+		logger := zerodriver.NewProductionLogger()
 		fmt.Printf("kbot %s started", appVersion)
 
 		kbot, err := telebot.NewBot(telebot.Settings{
@@ -42,17 +94,52 @@ to quickly create a Cobra application.`,
 		if err != nil {
 			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 			return
+		}	else {
+			logger.Info().Str("Version", appVersion).Msg("kbot started")
 		}
+		// kbot.Handle(telebot.OnText, func(m telebot.Context) error {
+		// 	log.Print(m.Message().Payload, m.Text())
+		// 	payload := m.Message().Payload
+
+		// 	switch payload {
+		// 	case "Hello Denny":
+		// 		err = m.Send(fmt.Sprintf("Hello! I`m Kbot %s. How are you up?", appVersion))
+
+		// 	}
+		trafficSignal := make(map[string]map[string]int8)
+
+		trafficSignal["red"] = make(map[string]int8)
+		trafficSignal["amber"] = make(map[string]int8)
+		trafficSignal["green"] = make(map[string]int8)
+
+		trafficSignal["red"]["pin"] = 12
+		trafficSignal["amber"]["pin"] = 27
+		trafficSignal["green"]["pin"] = 22
+
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
-			log.Print(m.Message().Payload, m.Text())
+			logger.Info().Str("Payload", m.Text()).Msg(m.Message().Payload)
+
 			payload := m.Message().Payload
+			pmetrics(context.Background(), payload)
 
 			switch payload {
-			case "Hello Denny":
-				err = m.Send(fmt.Sprintf("Hello! I`m Kbot %s. How are you up?", appVersion))
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+
+			case "red", "amber", "green":
+
+				if trafficSignal[payload]["on"] == 0 {
+					trafficSignal[payload]["on"] = 1
+				} else {
+					trafficSignal[payload]["on"] = 0
+				}
+
+				err = m.Send(fmt.Sprintf("Switch %s light signal to %d", payload, trafficSignal[payload]["on"]))
+
+			default:
+				err = m.Send("Usage: /s red|amber|green")
 
 			}
-
 			return err
 		})
 		kbot.Start()
@@ -71,4 +158,23 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func init() {
+	ctx := context.Background()
+	initMetrics(ctx)
+	rootCmd.AddCommand(kbotCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// kbotCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// kbotCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// Initialize OpenTelemetry tracer
+
 }
